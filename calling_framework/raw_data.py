ef8f3f06ae8da53af2227798791ec8265cf2c331
Gregor Sturm <mail@gregor-sturm.de>
2015-07-13 09:51:41 +0200

    initialized analysis framework

---
+++ calling_framework/raw_data.py
@@ -0,0 +1,28 @@
+"""module provides functions to get raw data from nanopore sequencing.
+Usually, data is provided in form of a .fast5-file; however sometimes
+one might encounter Robjects or other data-types from foreign sources.
+"""
+
+import numpy as np
+import h5py
+import rpy2.robjects as robjects
+
+def raw_from_fast5(path, channels):
+    """ read the raw data from a fast5 file.
+
+    Args:
+       path: path to file
+       channels: list with channel-ids to extracts
+
+    Returns:
+        dict[channel_id] -> np.array(raw_values)
+    """
+
+    f = h5py.File(path, r)
+    raw_data = dict()
+    for i in channels:
+        raw_data[i] = np.array(f["/Raw/Channel_{0}/Signal".format(i)])
+
+    return raw_data
+
+
