02c81c658a6bbc887313a89137e9cc954af94d50
Gregor Sturm <mail@gregor-sturm.de>
2015-09-14 13:57:46 +0200

    re-init git because of file-structure. hmm works in general

---
+++ 03_pipeline/make_events.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.insert(0, "../../nbwrapper")
+from nbwrapper import Nbwrapper
+import argparse
+import os
+
+if __name__ == "__main__":
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--f5_path", nargs="?", help="path to folder containing processed fast5-files")
+    argp.add_argument("--output", nargs="?", help="path to output file")
+    argp.add_argument("--ncores", nargs="?", default="4", help="number of CPU cores to use")
+
+    args =  argp.parse_args()
+    print(args)
+    path = os.path.dirname(os.path.realpath(__file__))
+    nb = Nbwrapper(args, path + "/make_events.ipynb")
+    nb.run()
+
+
