8624418926c860131e6104b1929bed21a6be5e56
Gregor Sturm <mail@gregor-sturm.de>
2015-09-15 13:56:42 +0200

    alignment-lib and various transition matrices

--- 03_pipeline/make_events.py
+++ 03_pipeline/make_events.py
@@ -10,7 +10,7 @@ import os
 if __name__ == "__main__":
     argp = argparse.ArgumentParser()
     argp.add_argument("--f5_path", nargs="?", help="path to folder containing processed fast5-files")
-    argp.add_argument("--output", nargs="?", help="path to output file")
+    argp.add_argument("--output_basename", nargs="?", help="path to output file")
     argp.add_argument("--ncores", nargs="?", default="4", help="number of CPU cores to use")
 
     args =  argp.parse_args()
